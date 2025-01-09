539af0e49079972c46fff62839784202620739d0
git-ksupriya <ksupriya2430@gmail,com>
2025-01-09 11:50:40 +0530

    connected sql with python

--- library_management_system.py
+++ library_management_system.py
@@ -1,1 +1,7 @@
+import mysql.connector as ctr
+
+conn=ctr.connect(host='localhost', user='root', password='1232')
+if conn.is_connected():
+    print('hello, it\'s connected!')
+
 print("hello world")
